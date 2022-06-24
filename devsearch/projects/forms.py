3a1f2b7e71663035c579831945d7c5c6a37c9c25
Yalda <yalda.ef@gmail.com>
2022-06-24 09:43:42 +0430

    CRUD

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -4,4 +4,4 @@ from . models import Project
 class ProjectForm(ModelForm):
     class Meta: # Meta class is used to specify various options for this model form.
         model = Project
-        fields = '__all__' # This will include all the fields in the model.
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,5 @@
 from pydoc import describe
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -16,6 +16,33 @@ def project(request, pk):
 
 
 def createProject(request):
-    form = ProjectForm(request.POST or None)
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     contex = {'form' : form}
     return render(request, 'projects/project_form.html', contex)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    contex = {'form' : form}
+    return render(request, 'projects/project_form.html', contex)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    contex = {'object' : project}
+    return render(request, 'projects/delete_template.html', contex)
